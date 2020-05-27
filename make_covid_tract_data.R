b3cb988ba849f76e807082c32c1ae6704c82b4a3
Erika <erika.rasnick@gmail.com>
2020-05-27 09:49:57 -0400

    add exisiting test sites and drive/walk buffers

--- make_covid_tract_data.R
+++ make_covid_tract_data.R
@@ -150,3 +150,49 @@ aws.s3::s3saveRDS(shelters, "s3://geomarker/covid_testing_locations/shelters.rds
 cmha <- read_csv('./data/cmha_addresses_geocoded.csv')
 aws.s3::s3saveRDS(cmha, "s3://geomarker/covid_testing_locations/cmha.rds")
 
+health_centers <- read_csv('./data/CHD Health Center addresses with zip_geocoded.csv')
+aws.s3::s3saveRDS(health_centers, "s3://geomarker/covid_testing_locations/health_centers.rds")
+
+library(openrouteservice)
+# download isochrones from ORS
+get_isochrones <- function(x, data, trans_mode, time_sec) {
+  ors_isochrones(data[x, c('lon', 'lat')],
+                 profile = trans_mode,
+                 range = time_sec,          # 900 seconds = 15 minutes
+                 output = "sf") %>%
+    st_transform(3735)
+}
+
+
+health_center_isochrones_15min_drive_list <- CB::mappp(1:nrow(health_centers),
+                                                  ~get_isochrones(.x, data = health_centers,
+                                                                  trans_mode = 'driving-car',
+                                                                  time_sec = 900))
+
+health_center_isochrones_10min_walk_list <- CB::mappp(1:nrow(health_centers),
+                                                 ~get_isochrones(.x, data = health_centers,
+                                                                 trans_mode = 'foot-walking',
+                                                                 time_sec = 600))
+
+
+# bind into one sf data.frame
+unionize_isochrones <- function(iso_list) {
+  iso <- iso_list[[1]]
+  for (i in 2:length(iso_list)) {
+    iso <- rbind(iso, iso_list[[i]])
+  }
+  return(st_union(iso))
+}
+
+health_center_isochrones_15min_drive <- unionize_isochrones(health_center_isochrones_15min_drive_list)
+health_center_isochrones_10min_walk <- unionize_isochrones(health_center_isochrones_10min_walk_list)
+mapview::mapview(health_center_isochrones_15min_drive)
+mapview::mapview(health_center_isochrones_10min_walk)
+
+aws.s3::s3saveRDS(health_center_isochrones_15min_drive, "s3://geomarker/covid_testing_locations/health_center_isochrones_15min_drive.rds")
+aws.s3::s3saveRDS(health_center_isochrones_10min_walk, "s3://geomarker/covid_testing_locations/health_center_isochrones_10min_walk.rds")
+
+
+
+
+
